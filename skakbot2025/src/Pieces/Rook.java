ba1a24e0ea4da7b9b338e4c7e601a4b9e343325f
Keragh <147395749+Keragh@users.noreply.github.com>
2025-05-07 15:13:55 +0200

    made "Castling" and promotion on pawn on backline, also made it so you can choose black or white to play

--- skakbot2025/src/Pieces/Rook.java
+++ skakbot2025/src/Pieces/Rook.java
@@ -3,9 +3,11 @@ package Pieces;
 import javax.swing.*;
 
 public class Rook extends Piece {
+    private boolean hasMoved;
 
     public Rook(boolean isWhite, int row, int col) {
         super(isWhite, row, col);
+        this.hasMoved = false;
     }
 
     @Override
@@ -13,22 +15,33 @@ public class Rook extends Piece {
         return Util.PieceImageLoader.getPieceIcon("rook", isWhite);
     }
 
-
     @Override
     public boolean isValidMove(int newRow, int newCol, Piece[][] board) {
         if (row != newRow && col != newCol) return false;
-
         int rowStep = Integer.compare(newRow, row);
         int colStep = Integer.compare(newCol, col);
-        int r = row + rowStep;
-        int c = col + colStep;
-
+        int r = row + rowStep, c = col + colStep;
         while (r != newRow || c != newCol) {
             if (board[r][c] != null) return false;
-            r += rowStep;
-            c += colStep;
+            r += rowStep; c += colStep;
         }
+        return board[newRow][newCol] == null || board[newRow][newCol].isWhite() != isWhite;
+    }
 
-        return board[newRow][newCol] == null || board[newRow][newCol].isWhite() != this.isWhite();
+    // Kun opdatering af koordinater til simulationer
+    @Override
+    public void setPosition(int newRow, int newCol) {
+        super.setPosition(newRow, newCol);
+    }
+
+    // Rigtigt træk – sæt hasMoved
+    @Override
+    public void move(int newRow, int newCol) {
+        super.setPosition(newRow, newCol);
+        hasMoved = true;
+    }
+
+    public boolean hasMoved() {
+        return hasMoved;
     }
 }
